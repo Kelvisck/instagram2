db08c43addced59fe5a4b7d29a0c6de166e76a70
kelvisck2 <kelvisck.cabral@ufms.br>
2024-05-20 23:05:17 -0400

    configuraçoes iniciais

---
+++ app/src/main/java/com/example/instagram2/activty/LoginActvity.kt
@@ -0,0 +1,13 @@
+package com.example.instagram2.activty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagram2.R
+
+class LoginActvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_actvity)
+    }
+
+}
--- app/src/main/java/com/example/instagram2/activty/MainActivity.kt
+++ app/src/main/java/com/example/instagram2/activty/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.instagram2
+package com.example.instagram2.activty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.instagram2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
